9e74857f5cb6c664b25d9c7273911772005eb5f5
Ion Dodon <iondodon2@gmail.com>
2021-02-28 21:31:19 +0200

    build_deploy.yml

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ ufw allow 5000
 
 docker login -u iondodon -p Eronat98
 docker pull iondodon/lrmodel:stage-v1.0
-docker run --name lrmodel -p 5000:5000 lrmodel
+docker run --name lrmodel -p 5000:5000 iondodon/lrmodel:stage-v1.0
